4ebce539b72a240b12ba38d463b76b42d90f031e
Nick <bogdanovichkolia1990@gmail.com>
2025-03-10 15:34:32 +0200

    done js for gallery descedits

--- src/main.js
+++ src/main.js
@@ -985,8 +985,8 @@ galleryList.addEventListener('click', function (event) {
 		const imgNumber = event.target.src.split('-')[1].split('.')[0];
 
 		const img = document.createElement('img');
-		img.src = `/img/galery/gallery-modal/gallery-modal-${imgNumber}.jpg`;
-		img.srcset = `/img/galery/gallery-modal/gallery-modal-${imgNumber}@2x.jpg 2x`;
+		img.src = `../img/galery/gallery-modal/gallery-modal-${imgNumber}.jpg`;
+		img.srcset = `../img/galery/gallery-modal/gallery-modal-${imgNumber}@2x.jpg 2x`;
 		img.alt = 'Nature in the Ukrainian mountains';
 		img.id = 'modal-image';
 		img.addEventListener('click', function () {
